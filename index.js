d95d064c2d5dd7782fffc4ec957a084f967ead2d
VincentLabor <vlabor95@gmail.com>
2021-03-09 12:32:12 -0800

    Renamed landing page file name

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
-let hbMenuClicked = document.getElementById("hbMenu")
+let hbMenuClicked = document.getElementById("hbMenu");
+let mobileNav = document.getElementById("mobileNav");
+let landingDiv = document.getElementById("landing");
+let navExit = document.getElementById("exitIcon");
 
-let feeder = () =>{
-    console.log("please work")
+
+let toggleMobileNav = () =>{
+    mobileNav.classList.remove("hidden");
+    landingDiv.classList.add("hidden")
 }
 
-hbMenuClicked.addEventListener("click",feeder);
+let exitMobileNav = ()=>{
+    mobileNav.classList.add("hidden");
+    landingDiv.classList.remove("hidden");
+}
+
+// These wait and listen for the user to do these actions. 
+hbMenuClicked.addEventListener("click",toggleMobileNav);
+navExit.addEventListener("click",exitMobileNav)
